e00efc817921a68395698ff61172b193570b927c
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-06-02 23:40:38 +0800

    scramblies added

---
+++ scramblies.js
@@ -0,0 +1,15 @@
+function scramble(str1, str2) {
+    const op2 = str1.split('');
+
+    for (let char of str1) {
+        if (op2.includes)
+    }
+}
+
+console.log(scramble('rkqodlw', 'world'));
+console.log(scramble('cedewaraaossoqqyt', 'codewars'));
+console.log(scramble('katas', 'steak'));
+
+// Pseducode:
+// return true if the portion of str1 can be rearrange as str2, else return false
+// if char in str1 includes all item/char in str2, return true, else false
