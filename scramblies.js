db290c49bfa2ab2f29831c6d7b4931e764489c22
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-06-02 23:51:35 +0800

    scramblies, modified

--- scramblies.js
+++ scramblies.js
@@ -1,14 +1,17 @@
 function scramble(str1, str2) {
-    const op2 = str1.split('');
+    const arr1 = str1.split('');
 
-    for (let char of str1) {
-        if (op2.includes)
-    }
+    for (let char of str2) {
+        if (!arr1.includes(char)) {
+            return false;
+        }
+    } return true;
 }
 
 console.log(scramble('rkqodlw', 'world'));
 console.log(scramble('cedewaraaossoqqyt', 'codewars'));
 console.log(scramble('katas', 'steak'));
+console.log(scramble('scriptjavx', 'javascript'))
 
 // Pseducode:
 // return true if the portion of str1 can be rearrange as str2, else return false
