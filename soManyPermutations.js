121f06c06e697f03d15ce99d658d27020cb24d86
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-06-07 21:31:20 +0800

    modified

--- soManyPermutations.js
+++ soManyPermutations.js
@@ -1,24 +1,23 @@
 function permutations(str) {
     const arr = str.split('');
-    let noDuplicate = [];
-    
-    // Remove duplicate
-    for (let i = 0; i < arr.length; i++) {
-        if (!noDuplicate.includes(arr[i])) {
-            noDuplicate.push(arr[i]);
-        }
-    }
 
-    // 
+    let result = [];
+    
+    for (let i = 0; i < str.length; i++) {
+        result.push(str[i]);
+    }
+    return result;
 }
-console.log(permutations('abbbc'));
+console.log(permutations('abc'));
 
 // Pseducode
     // Goal: create a permutation of strings (all possible orderings for the given string.)
     // Input: 'ab'
     // Output: ['ab', 'ba']
     // Note:
-        // # no duplicate letters
+        // # no duplicate array
 
-    // Remove duplicates from str,
-    // 
+    // Loop str to select each item, pair each item all 
+    // select one
+    
+// https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/javascript
