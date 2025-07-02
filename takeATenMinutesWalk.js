011d89e7bd06d7e3919a1a8f4bc95b27306aa6ea
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-02 20:55:00 +0800

    done, short and simple

--- takeATenMinutesWalk.js
+++ takeATenMinutesWalk.js
@@ -1,26 +1,26 @@
 function isValidWalk(walk) {
-    let count = 0;
+    if (walk.length !== 10) return false;
 
-    for (let i = 0; i < walk.length; i++) {
-        if (i === 0) {
-            count++;
-        } else if (walk[i] === walk[i - 1]) {
-            count--;
-        } else {
-            count++;
-        }
+    let x = 0;
+    let y = 0;
+
+    for (let dir of walk) {
+        if (dir === 'n') y++;
+        if (dir === 's') y--;
+        if (dir === 'e') x++;
+        if (dir === 'w') x--;
     }
-    
-    if (count === 10) return true;
-    return false;
+
+    return x === 0 && y === 0;
 }
-//let curr = walk[i - 1];
 
 console.log(isValidWalk(['n', 's', 'w', 'e'])); // return false
-console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])); // return true
-console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])); // return fasle
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])); // return false
+console.log(isValidWalk(['n','n','n','n','n','e','e','e','e','w'])); // true
+console.log(isValidWalk(['n','n','n','s','n','s','n','s','n','s'])); // return faLse
+console.log(isValidWalk(['n','s','e','w','n','s','e','w','n','s']))
 
 // Pseducode:
 //  Goal:   return true if the person walks 10 mins on time, else return false less than or more than 10 mins.
 // Assumes that 1 block takes 1 minute to walk.
-// Count how many times the person walks and change direction 1mins per 1block, if the person walks to the same direction, it means he stays in the same spot then (decrement count), else (increment). Return true if count walks is equal to 10, else, return false.
+// Count how many times the person walks and change direction 1mins per 1block, if the person walks back (eg. n to s) (decrement count), else (increment). Return true if count walks is equal to 10, else, return false.
