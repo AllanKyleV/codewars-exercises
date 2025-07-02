701c6d74e2b390f0dc8c3203fa4d8e859bd39e49
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-03 06:19:04 +0800

    done with recursion

--- arrayDeepCount.js
+++ arrayDeepCount.js
@@ -1,8 +1,24 @@
 function deepCount(a) {
-    // base case
-    if (a.length <= 1) return;
-    console.log(a)
-    return a + deepCount(a - 1);
+    if (a.length === 0) return 0;
+
+    let count = 0;
+    for (let item of a) {
+        if (Array.isArray(item)) {  
+            count += deepCount(item);
+        }
+        count++;
+    }
+    return count;
 }
 
-console.log(deepCount([[[]]])); // Count nested array, should return 3
+console.log(deepCount([[[]]]));
+console.log(deepCount([]));
+console.log(deepCount([1, 2, 3]));
+console.log(deepCount([1, 2, [3, 4, [5]]]));
+
+// Pseducode:
+//  Goal: return the number of all elements inclucding nested array within the array.
+// Input:   [], return 0
+//          [1, 2, 3], return 3
+//          [1, 2, [3, 4, [5]]], return 7
+// Write conditionalas for: return 0 if the array is empty, count and retrun the number of elements within array including the nested array. If the item is an array, add the count of the inner elements (by recursion);
