47cd865d65adb47a7eb3a018036c38a0a8cdc237
Allan Kyle <199327511+AllanKyleV@users.noreply.github.com>
2025-07-05 17:50:59 +0800

    modified

--- camelCaseMethod.js
+++ camelCaseMethod.js
@@ -1,22 +1,24 @@
-String.prototype.camelCase = function(str) {
-    let arr = str.split(' ');
+String.prototype.camelCase = function() {
+    let arr = this.split(' ');
     let result = [];
 
     for (let char of arr) {
+        let word = [];
+
         for (let letter of char) {
-            let word = [];
             if (letter === char[0]) {
                 word.push(letter.toUpperCase());
             } else {
-                word.push(letter);
+                word.push(letter.toLowerCase());
             }
-            result.push(word);
         }
+
+        result.push(word.join(''));
     }
-    return result;
+    return result.join('');
 }
 
-console.log(String.prototype.camelCase('camel case word'));
+'hello case'.camelCase();
 
 // Pseducode:
 //  Goal: convert strings into Camel Case (the first letter of each word is capitalized).
